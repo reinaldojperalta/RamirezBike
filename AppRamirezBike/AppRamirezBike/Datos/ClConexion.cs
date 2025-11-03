38e3d0f1bcfc38b7713ea82cef71d9531a81564b
reinaldo-peralta <rjperalta07@soy.sena.edu.co>
2025-11-03 14:46:49 -0500

    creando la clae de conexion

--- AppRamirezBike/AppRamirezBike/Datos/ClConexion.cs
+++ AppRamirezBike/AppRamirezBike/Datos/ClConexion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,21 @@ namespace AppRamirezBike.Datos
 {
 	public class ClConexion
 	{
-	}
+        SqlConnection objConexion;
+
+        public ClConexion()
+        {
+            objConexion = new SqlConnection("Data Source=DESKTOP-BTOUG7O;Initial Catalog=dbRamirezBike;Integrated Security=True");
+        }
+        public SqlConnection MtAbrirConexion()
+        {
+            objConexion.Open();
+            return objConexion;
+        }
+
+        public void MtCerrarConexion()
+        {
+            objConexion.Close();
+        }
+    }
 }
