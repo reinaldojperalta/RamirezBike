2dfcd7843c4d0f4dda4f14f8a080028c0ffb561b
juliansaaenz <julianstivensaenzmontanez@gmail.com>
2025-11-04 09:14:42 -0500

    Se creo Modelo Y Clases De Usuario En Las Capas De Datos Y Logica

---
+++ AppRamirezBike/AppRamirezBike/Datos/ClConexion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AppRamirezBike.Datos
+{
+    public class ClConexion
+    {
+        SqlConnection oConexion;
+
+        public ClConexion()
+        {
+            oConexion = new SqlConnection("");
+        }
+        public SqlConnection MtAbrirConexion()
+        {
+            oConexion.Open();
+            return oConexion;
+        }
+        public void MtCerrarConexion()
+        {
+            oConexion.Close();
+        }
+    }
+}
---
+++ AppRamirezBike/AppRamirezBike/Logica/ClUsuarioL.cs
@@ -0,0 +1,19 @@
+﻿using AppRamirezBike.Datos;
+using AppRamirezBike.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppRamirezBike.Logica
+{
+	public class ClUsuarioL
+	{
+        public bool MtRegistrarL(ClUsuario oUsuario)
+        {
+            ClUsuarioD oUsuarioD = new ClUsuarioD();
+            int filas = oUsuarioD.MtRegistrarUsuario(oUsuario);
+            return filas == 1;
+        }
+    }
+}
