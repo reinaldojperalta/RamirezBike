771f0ef0bf53f21b26c7ec2b7d4d5af6da1ad0e9
reinaldo-peralta <rjperalta07@soy.sena.edu.co>
2025-11-06 20:17:57 -0500

    se relizo la paginacion quedan pendiente los estados

--- AppRamirezBike/AppRamirezBike/Vista/Catalogo.aspx.designer.cs
+++ AppRamirezBike/AppRamirezBike/Vista/Catalogo.aspx.designer.cs
@@ -22,5 +22,14 @@ namespace AppRamirezBike.Vista
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Repeater rptProducto;
+
+        /// <summary>
+        /// Control rptPaginacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptPaginacion;
     }
 }
