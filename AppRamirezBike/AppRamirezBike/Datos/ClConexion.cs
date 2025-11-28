537404bb8aab84be367dfa01db3c7612d39215ac
SantiagoBecerra <Santiago603@gmail.com>
2025-11-28 09:06:24 -0500

    Comprobado Flujo Completo De La Pagina En Local

--- AppRamirezBike/AppRamirezBike/Datos/ClConexion.cs
+++ AppRamirezBike/AppRamirezBike/Datos/ClConexion.cs
@@ -17,6 +17,8 @@ namespace AppRamirezBike.Datos
 
 
             objConexion = new SqlConnection(ConfigurationManager.ConnectionStrings["dbRamirezBikeConnection"].ConnectionString);
+            //objConexion = new SqlConnection("Data Source=DESKTOP-8842IPF;Initial Catalog=dbRamirezBike;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+        
         }
         public SqlConnection MtAbrirConexion()
         {
